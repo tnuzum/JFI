768a16d596c2b30d4f27dbf0c39c48134e42799d
bhagyakalle <rayaprolub@gmail.com>
2019-11-14 19:58:56 -0500

    MakePaymentTest updated

--- src/test/java/EME/MakePaymentTest.java
+++ src/test/java/EME/MakePaymentTest.java
@@ -56,6 +56,7 @@ public class MakePaymentTest extends base {
 		reusableWaits.waitForAcceptButton();
 		p.getPopupConfirmationButton().click();
 		reusableMethods.returnToDashboard();
+		reusableWaits.waitForDashboardLoaded();
 	}
 
 	@Test(priority = 2, description = "Confirming payment is applied")
