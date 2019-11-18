220cd9698fee41ae9445315feb946c00cc3b6179
tnuzum <todd.nuzum@jonasfitness.com>
2019-11-18 17:17:12 -0500

    JOL test updates

--- src/test/java/JOL/LandingPageTest.java
+++ src/test/java/JOL/LandingPageTest.java
@@ -38,8 +38,13 @@ public class LandingPageTest extends base{
 		Assert.assertEquals("Webster, TX", l.getClub1Location().getText());
 		Assert.assertEquals("Jonas Sports-Plex", l.getClub1AddressTitle().getText());
 		Assert.assertTrue(l.getClub1Address().getText().contains("16969 S Texas Ave"));
-
-//		System.out.println(l.getClub1Address().getText());
+		Assert.assertTrue(l.getClub1Address().getText().contains("Webster, TX, 77598"));
+		Assert.assertTrue(l.getClub1Address().getText().contains("Phone : (425) 998-3435"));
+		Assert.assertTrue(l.getClub1Address().getText().contains("Email : stacie.coates@jonasfitness.com"));
+		Assert.assertTrue(l.getClub1ViewPlansButton().isEnabled());
+		l.getClub1ViewPlansButton().click();
+		
+		//System.out.println(l.getClub2Address().getText());
 
 		}
 	@Test (priority = 3)
@@ -50,6 +55,10 @@ public class LandingPageTest extends base{
 		Assert.assertEquals("Webster, TX", l.getClub2Location().getText());
 		Assert.assertEquals("Studio Jonas", l.getClub2AddressTitle().getText());
 		Assert.assertTrue(l.getClub2Address().getText().contains("1 Nasa Pkwy"));
+		Assert.assertTrue(l.getClub2Address().getText().contains("Webster, TX, 77598"));
+		Assert.assertTrue(l.getClub2Address().getText().contains("Phone : (425) 998-3435"));
+		Assert.assertTrue(l.getClub2Address().getText().contains("Email : Stacie.Coates@Jonasfitness.com"));
+		Assert.assertTrue(l.getClub2ViewPlansButton().isEnabled());
 
 		}
 
