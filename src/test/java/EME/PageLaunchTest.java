500a1f730f4419739ac7262f4e633db818854fe8
bhagyakalle <rayaprolub@gmail.com>
2019-11-19 22:01:36 -0500

    Update PageLaunchTest.java

--- src/test/java/EME/PageLaunchTest.java
+++ src/test/java/EME/PageLaunchTest.java
@@ -108,6 +108,11 @@ public class PageLaunchTest extends base{
 //		log.info("Appointments Page Header Verified");
 //		d.getDashboardButton().click();
 		d.getMenuMyActivies().click();//Accessing from left pane menu
+		while (!d.getmenuMyActivitiesSubMenu().getAttribute("style").contains("1"))
+		{
+			Thread.sleep(1000);
+		}
+		
 		d.getMenuBookAppointment().click();
 // The pageHeader changed in 7.28
 //		Assert.assertEquals(a.getPageHeader().getText(),"Appointments");
