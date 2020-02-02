8b458ebdc3de61f2026ada250b292f40dadf155e
bhagyakalle <rayaprolub@gmail.com>
2020-02-02 12:48:31 -0500

    added profile in pom

--- src/test/java/EME/MakeApptTest.java
+++ src/test/java/EME/MakeApptTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -119,7 +120,9 @@ private static Logger log =LogManager.getLogger(base.class.getName());
 					}
 					Boolean TomorrowDatePresent = reusableMethods.isElementPresent(By.xpath("//mwl-calendar-month-cell[contains(@class,'future')])[1]"));
 					if (TomorrowDatePresent == false)
-					{driver.findElement(By.xpath("//i[contains(@class, 'right')]")).click();
+					{
+						
+						 driver.findElement(By.xpath("//i[contains(@class, 'right')]")).click();;
 
 					   result1 = reusableWaits.loadingAvailability();
 						if (result1 == true)
