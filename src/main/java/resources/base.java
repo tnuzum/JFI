d5ac6ab139d4a6f6cdc4fa1ba2c61ad12d54dfee
bhagyakalle <54186152+bhagyakalle@users.noreply.github.com>
2020-05-16 02:12:52 -0400

    added window sizing and other chrome options

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -15,6 +15,7 @@ import java.util.concurrent.TimeUnit;
 import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.TakesScreenshot;
@@ -98,6 +99,7 @@ public class base {
 					log.info("Chrome Browser: Running Tests on local machine");
 					ChromeOptions co = new ChromeOptions();
 					co.addArguments("--start-maximized");
+					co.addArguments("--window-size=1920, 1080");
 					co.merge(dcch);
 					System.setProperty("webdriver.chrome.driver", "C:\\Automation\\libs\\webdrivers\\chromedriver.exe");
 					if (browserName.contains("headless")) {
@@ -145,13 +147,12 @@ public class base {
 		ssTime = dateFormat2.format(date2);
 
 		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
 
-		/*
-		 * Dimension dimension = new Dimension(1920, 1080);
-		 * driver.manage().window().setSize(dimension);
-		 */
+		Dimension dimension = new Dimension(1920, 1080);
+		driver.manage().window().setSize(dimension);
+
 		return driver;
 	}
 
