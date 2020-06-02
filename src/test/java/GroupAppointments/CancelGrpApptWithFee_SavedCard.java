95bd172a47eb3ea891998d1d97813fe949c14397
bhagyakalle <54186152+bhagyakalle@users.noreply.github.com>
2020-06-02 00:31:09 -0400

    Test results and screenshots will now include the classname.methodname

--- src/test/java/GroupAppointments/CancelGrpApptWithFee_SavedCard.java
+++ src/test/java/GroupAppointments/CancelGrpApptWithFee_SavedCard.java
@@ -49,7 +49,8 @@ public class CancelGrpApptWithFee_SavedCard extends base {
 		driver = initializeDriver();
 		rm.setDriver(driver);
 		rw.setDriver(driver);
-		log.info("Driver Initialized");
+		log.info("Driver Initialized for " + this.getClass().getSimpleName());
+		System.out.println("Driver Initialized for " + this.getClass().getSimpleName());
 		driver.get(prop.getProperty("EMELoginPage"));
 	}
 
