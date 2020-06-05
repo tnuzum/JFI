952291e4544b4befd6e28233de7fc19afee0023f
bhagyakalle <54186152+bhagyakalle@users.noreply.github.com>
2020-06-05 13:17:09 -0400

    more clean up

--- src/test/java/PayBalance/MakePaymentTest_NewCard_HasAgreement.java
+++ src/test/java/PayBalance/MakePaymentTest_NewCard_HasAgreement.java
@@ -60,13 +60,15 @@ public class MakePaymentTest_NewCard_HasAgreement extends base {
 
 	@Test(priority = 1, description = "Adding $5.00 to member's account")
 	public void MakePaymentWithNewCard() throws InterruptedException, IOException {
-		rm.activeMemberLogin("dauto", "Testing1!");
-		rw.waitForDashboardLoaded();
+
 		DashboardPO d = new DashboardPO(driver);
 		PaymentPO p = new PaymentPO(driver);
-		d.getMyAccountPayNow().click();
-
 		try {
+			rm.activeMemberLogin("dauto", "Testing1!");
+			rw.waitForDashboardLoaded();
+
+			d.getMyAccountPayNow().click();
+
 			WebDriverWait wait = new WebDriverWait(driver, 10);
 			wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//h2[@class='text-center']")));
 
