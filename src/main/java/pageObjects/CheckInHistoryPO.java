5b1020b7da2e8a32b37a45b0ba8d0157bf8b9042
bhagyakalle <54186152+bhagyakalle@users.noreply.github.com>
2020-06-12 00:12:35 -0400

    Update CheckInHistoryPO.java

--- src/main/java/pageObjects/CheckInHistoryPO.java
+++ src/main/java/pageObjects/CheckInHistoryPO.java
@@ -1,5 +1,7 @@
 package pageObjects;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,6 +14,13 @@ public class CheckInHistoryPO {
 
 //	By pageHeader = By.xpath("//div[@class='col-sm-12']/h2");
 	By pageHeader = By.xpath("//h2[@class = 'at-breadcrumb-title']");
+	By calendarIcons = By.xpath("//mat-datepicker-toggle[@class= 'mat-datepicker-toggle']");
+	By firstCalendarDates = By.xpath("//div[contains(@class, 'cdk-overlay-pane')] //td");
+	By secondCalendarDates = By.xpath("//div[contains(@class, 'cdk-overlay-pane')] //td");
+	By visitsByMonthSection = By.xpath("//div[@class='ibox-content no-margins no-padding']");
+	By checkinHistorySection = By.xpath("//div[@id='checkinHistory']");
+	By rightCalendarArrow = By.xpath("//button[@class='mat-calendar-next-button mat-icon-button']");
+	By noEntriesMessage = By.xpath("//div[@class='panel-body no-padding']");
 
 // CONSTRUCTOR
 
@@ -24,4 +33,35 @@ public class CheckInHistoryPO {
 		return driver.findElement(pageHeader);
 	}
 
+	public WebElement getFirstCalendarIcon() {
+		return driver.findElements(calendarIcons).get(0);
+	}
+
+	public WebElement getSecondCalendarIcon() {
+		return driver.findElements(calendarIcons).get(1);
+	}
+
+	public List<WebElement> getFirstCalendarDates() {
+		return driver.findElements(firstCalendarDates);
+	}
+
+	public List<WebElement> getSecondCalendarDates() {
+		return driver.findElements(secondCalendarDates);
+	}
+
+	public WebElement getVisitsByMonthSection() {
+		return driver.findElement(visitsByMonthSection);
+	}
+
+	public WebElement getCheckinHistorySection() {
+		return driver.findElement(checkinHistorySection);
+	}
+
+	public WebElement getRightCalendarArrow() {
+		return driver.findElement(rightCalendarArrow);
+	}
+
+	public WebElement getNoEntriesMessage() {
+		return driver.findElement(noEntriesMessage);
+	}
 }
