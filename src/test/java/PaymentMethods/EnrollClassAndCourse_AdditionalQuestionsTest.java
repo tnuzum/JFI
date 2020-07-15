c4634074102c6bede57ef1dcb0e7442bf876f5ff
bhagyakalle <54186152+bhagyakalle@users.noreply.github.com>
2020-07-15 19:22:35 -0400

    changes

--- src/test/java/PaymentMethods/EnrollClassAndCourse_AdditionalQuestionsTest.java
+++ src/test/java/PaymentMethods/EnrollClassAndCourse_AdditionalQuestionsTest.java
@@ -329,7 +329,7 @@ public class EnrollClassAndCourse_AdditionalQuestionsTest extends base {
 	public void deleteCardInCOG() throws InterruptedException, IOException {
 		try {
 
-			rm.deleteFOPInCOG("1143354", "Jonas Sports-Plex", "1111", "No");
+			rm.deleteFOPInCOG("1143515", "Jonas Sports-Plex", "1111", "No");
 
 		} catch (java.lang.AssertionError ae) {
 			System.out.println("assertion error");
