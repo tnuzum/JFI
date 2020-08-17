361455a5b352481c1892bccdf2d0628d0bf04f6e
bhagyakalle <54186152+bhagyakalle@users.noreply.github.com>
2020-08-17 01:16:37 -0400

    Added the Calendar layout and unenroll tests to TestNg xml file

--- src/main/java/pageObjects/CalendarPO.java
+++ src/main/java/pageObjects/CalendarPO.java
@@ -19,6 +19,7 @@ public class CalendarPO {
 	By rightArrow = By.xpath("//div[@class = 'btn-group']//div[contains(@class, 'btn-white')][3]");
 	By calDayBadge = By.xpath("//span[contains(@class, 'cal-day-badge')]");
 	By calEventTitle = By.xpath("//span[contains(@class, 'cal-event-title')]");
+	By classDetailPopup = By.xpath("//div[@class='modal-content']");
 	By unEnrollBtn = By.xpath("//div[contains(text(), 'unenroll')]");
 	By cancelBtn = By.xpath("//div[contains(text(), 'cancel')]");
 	By addToCalendarBtn = By.xpath("//button[contains(text(), 'add to calendar')]");
@@ -32,6 +33,40 @@ public class CalendarPO {
 	By type = By.xpath("//div[@class='modal-body']//small[3]");
 	By instructor = By.xpath("//div[@class='modal-body']//small[4]");
 	By club = By.xpath("//div[@class='modal-body']//small[5]");
+	By calendar = By.xpath("//div[@ng-reflect-ng-switch = 'month']");
+	By calendarList = By.xpath("//div[@class= 'panel panel-default m-t-md ng-star-inserted']");
+	By calendarHistory = By.xpath("//div[@class= 'panel panel-default m-t-md ng-star-inserted']");
+	By calendarViewLink = By.xpath("//li[@class = 'nav-item']/a");
+	By calendarListViewLink = By.xpath("//li[@class = 'nav-item']/a");
+	By calendarHistoryLink = By.xpath("//li[@class = 'nav-item']/a");
+	By monthButton = By.xpath("//div[@id='monthViewBtn']");
+	By weekButton = By.xpath("//div[contains(text(), 'Week')]");
+	By clubDropdown = By.xpath("//div[contains(@class,'row-box p-sm')]//select[@name='clubs']");
+	By additionalFilters = By.xpath("//a[@class='add-filters']//div");
+	By classCourseApptChkBoxes = By.xpath("//div[@class = 'checkbox checkbox-primary']");
+	By familyMemberNames = By.id("checkbox-colored-list");
+	By selectFamilyLabel = By.xpath("//small[contains(text(),'SELECT FAMILY')]");
+	By applyFiltersLink = By.linkText("Apply Filters");
+	By memberClassDetails = By.xpath("//div[@id='listview']//div[4]");
+	By classGearButton = By.xpath("//i[@class='fa fa-gear pull-right m-r-xs ng-star-inserted']");
+	By addToCalButtonListView = By.xpath("//button[contains(text(), 'ADD TO CALENDAR')]");
+	By unenrollListview = By.xpath("//a[contains(text(), 'UNENROLL')]");
+
+	public WebElement getMemberClassDetails() {
+		return driver.findElement(memberClassDetails);
+	}
+
+	public WebElement getClassGearButton() {
+		return driver.findElement(classGearButton);
+	}
+
+	public WebElement getAddToCalButtonListView() {
+		return driver.findElement(addToCalButtonListView);
+	}
+
+	public WebElement getUnenrollListview() {
+		return driver.findElement(unenrollListview);
+	}
 
 // CONSTRUCTOR
 
@@ -62,6 +97,10 @@ public class CalendarPO {
 
 	public WebElement getCalEventTitle() {
 		return driver.findElement(calEventTitle);
+	}
+
+	public WebElement getClassDetailPopup() {
+		return driver.findElement(classDetailPopup);
 	}
 
 	public List<WebElement> getCalEventTitles() {
@@ -118,5 +157,61 @@ public class CalendarPO {
 
 	public WebElement getAddToCalendarBtn() {
 		return driver.findElement(addToCalendarBtn);
+	}
+
+	public WebElement getCalendar() {
+		return driver.findElement(calendar);
+	}
+
+	public WebElement getCalendarList() {
+		return driver.findElements(calendarList).get(0);
+	}
+
+	public WebElement getCalendarHistory() {
+		return driver.findElements(calendarHistory).get(1);
+	}
+
+	public WebElement getCalendarViewLink() {
+		return driver.findElements(calendarViewLink).get(0);
+	}
+
+	public WebElement getCalendarListViewLink() {
+		return driver.findElements(calendarListViewLink).get(1);
+	}
+
+	public WebElement getCalendarHistoryLink() {
+		return driver.findElements(calendarHistoryLink).get(2);
+	}
+
+	public WebElement getMonthButton() {
+		return driver.findElement(monthButton);
+	}
+
+	public WebElement getWeekButton() {
+		return driver.findElement(weekButton);
+	}
+
+	public WebElement getAdditionalFilters() {
+		return driver.findElement(additionalFilters);
+	}
+
+	public WebElement getClubDropdown() {
+		return driver.findElement(clubDropdown);
+	}
+
+	public List<WebElement> getClassCourseApptChkBoxes() {
+		return driver.findElements(classCourseApptChkBoxes);
+	}
+
+	public List<WebElement> getFamilyMemberNames() {
+		return driver.findElements(familyMemberNames);
+	}
+
+	public WebElement getSelectFamilyLabel() {
+		return driver.findElement(selectFamilyLabel);
+	}
+
+	public WebElement getApplyFiltersLink() {
+		return driver.findElement(applyFiltersLink);
 	}
 }
