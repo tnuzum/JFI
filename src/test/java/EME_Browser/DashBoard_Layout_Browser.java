2a38b109fb8713ee9b2f182e09a0396aca3d815e
bhagyakalle <rayaprolub@gmail.com>
2020-11-09 08:52:43 -0500

    browser tests

--- src/test/java/EME_Browser/DashBoard_Layout_Browser.java
+++ src/test/java/EME_Browser/DashBoard_Layout_Browser.java
@@ -1,8 +1,12 @@
 package EME_Browser;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -26,6 +30,18 @@ public class DashBoard_Layout_Browser extends base2 {
 	@BeforeClass
 	@Parameters({ "Browser" })
 	public void initialize(String Browser) throws InterruptedException, IOException {
+
+		log.info(Browser + " Browser: Running Tests on Selenium Grid");
+
+		DesiredCapabilities dc = new DesiredCapabilities();
+		dc.setBrowserName(Browser); // dc.setPlatform(Platform.WINDOWS);
+
+		driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), dc);
+
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+
 		driver = initializeDriver(Browser);
 		rm.setDriver(driver);
 
