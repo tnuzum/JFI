2443de020183a294bb5977a8051dbc5ceb80257f
bhagyakalle <rayaprolub@gmail.com>
2021-02-03 00:48:48 -0500

    Update reusableMethods.java

--- src/main/java/resources/reusableMethods.java
+++ src/main/java/resources/reusableMethods.java
@@ -1174,7 +1174,7 @@ public class reusableMethods extends base {
 
 		driver.findElement(By.xpath("//i[@class='fa fa-calendar calenderbtn']")).click();
 		Select monthDropdown = new Select(driver.findElement(By.xpath("//select[@class='ui-datepicker-month']")));
-		monthDropdown.selectByVisibleText("Feb");
+		monthDropdown.selectByVisibleText("May");
 		Select yearDropdown = new Select(driver.findElement(By.xpath("//select[@class='ui-datepicker-year']")));
 		yearDropdown.selectByVisibleText("2021");
 		List<WebElement> Dates = driver.findElements(By.xpath("//table[@class='ui-datepicker-calendar'] //td/a"));
