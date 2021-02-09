a98019cec7b9dd3de456374e6744ad28255880f2
bhagyakalle <rayaprolub@gmail.com>
2021-02-09 12:41:50 -0500

    updates

--- src/test/java/GroupAppointments/ChangeGrpApptWithFee_ClubReqPackages_NewCard.java
+++ src/test/java/GroupAppointments/ChangeGrpApptWithFee_ClubReqPackages_NewCard.java
@@ -110,7 +110,7 @@ public class ChangeGrpApptWithFee_ClubReqPackages_NewCard extends base {
 					jse.executeScript("arguments[0].click();", AftrnunSlot);
 					Thread.sleep(1000);
 					WebElement AftrenoonAvailableTimeContainer = ap.getTimeSlotContainers().get(m)
-							.findElement(By.id("tab-2-1"));
+							.findElement(By.id("tab-2-2"));
 					List<WebElement> AftrenoonAvailableTimes = AftrenoonAvailableTimeContainer
 							.findElements(By.tagName("button"));
 					WebElement secondAvailableTimeAfternoon = AftrenoonAvailableTimes.get(1);
